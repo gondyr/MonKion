34296ed27843da77a3aa931327aa38c8a2bb8f87
gondy <gondyr@list.ru>
2024-09-16 20:32:36 +0400

    Vesion1

---
+++ .idea/HomeWork5/compNumb.java
@@ -0,0 +1,16 @@
+public class compNumb {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+
+    public static void compareNumbers() {
+        int a = 3; // Замените на любое значение
+        int b = 33; // Замените на любое значение
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
